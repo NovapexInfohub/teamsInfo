eb8c2afba4012ea1c66654ef411273313d8ca10d
nils121 <nilesh.aswar121@gmail.com>
2024-12-31 12:30:26 +0530

    initial commit

--- teamsInfo/src/createClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-
-export const supabase= createClient(
-    "https://ihznhwnfmvstrzgzfnvx.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imloem5od25mbXZzdHJ6Z3pmbnZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU1NjM1NzcsImV4cCI6MjA1MTEzOTU3N30.zKfCNmo_AYDssR_b6ydYcst5dDohP_5Gj3V4p65VXGQ"
-    )
